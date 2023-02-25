22333ae9a2597a2fedb8c36de857100e36c7f318
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-02-25 22:07:26 +0000

    added and completed Navbar as well as Mobile Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h2 className="text-5xl text-left">hello welcome</h2>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
